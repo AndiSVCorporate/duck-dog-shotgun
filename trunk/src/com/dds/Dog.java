23728d9ecbc046a040ae1347d42903a8bf8b81ff
wildewouter@gmail.com <wildewouter@gmail.com@bfb4acf6-1144-4334-fd50-431394f2b8de>
2012-09-19 14:35:28 +0000

    Update

--- trunk/src/com/dds/Dog.java
+++ trunk/src/com/dds/Dog.java
@@ -1,7 +1,8 @@
 package com.dds;
 
-import org.cocos2d.layers.CCScene;
+import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.CGSize;
 
 /**
  * @author Wouter
@@ -9,4 +10,14 @@ import org.cocos2d.nodes.CCSprite;
  * Time: 12:01
  */
 public class Dog extends CCSprite {
+
+    public Dog(String path) {
+        super(path);
+        CGSize winSize = CCDirector.sharedDirector().displaySize();
+
+        int x = (int) winSize.width /2;
+        int y = (int) this.getContentSize().getHeight() /2;
+
+        setPosition(x, y);
+    }
 }
