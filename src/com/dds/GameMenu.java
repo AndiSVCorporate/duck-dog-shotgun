4411ca88515351c8dc159abab52ecd6b75b5ae5d
wildewouter <wildewouter@gmail.com>
2012-10-08 11:04:21 +0000

    VANALLESENNOGWAT

--- src/com/dds/GameMenu.java
+++ src/com/dds/GameMenu.java
@@ -1,16 +1,22 @@
 package com.dds;
 
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 import org.cocos2d.layers.CCScene;
-import org.cocos2d.menus.CCMenu;
-import org.cocos2d.menus.CCMenuItemLabel;
+import org.cocos2d.menus.*;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.nodes.CCLabel;
 import org.cocos2d.nodes.CCNode;
+import org.cocos2d.nodes.CCSprite;
 import org.cocos2d.types.CGPoint;
+import org.cocos2d.types.CGSize;
 import org.cocos2d.types.ccColor3B;
 
 /**
  * @author Wouter
- *         Date: 04-10-12
- *         Time: 12:45
+ * Date: 04-10-12
+ * Time: 12:45
  */
 public class GameMenu extends CCMenu {
     public static CCScene scene() {
@@ -24,22 +30,19 @@ public class GameMenu extends CCMenu {
     }
 
     public GameMenu() {
-        CCNode ccStartNode = CCNode.node();
-        ccStartNode.setPosition(CGPoint.ccp(300, 300));
-        ccStartNode.setAnchorPoint(CGPoint.ccp(0, 0));
+        super();
+        CGSize winSize = CCDirector.sharedDirector().displaySize();
 
-        CCMenuItemLabel startGame = CCMenuItemLabel.item("text", this, "");
-        startGame.setPosition(CGPoint.ccp(0, 0));
-        startGame.setAnchorPoint(CGPoint.ccp(0, 0));
-        startGame.setColor(ccColor3B.ccc3(139, 69, 19));
-        startGame.setScale(0.5f);
+        CCMenuItemImage menuItem = CCMenuItemImage.item("button.png", "button_pressed.png", this, "startGame");
+        CCLabel playLabel = CCLabel.makeLabel("Play Game", "Arial", 40f);
+        playLabel.setPosition(menuItem.getContentSize().width/2, menuItem.getContentSize().height/2);
+        menuItem.addChild(playLabel);
 
-        CCMenu menu2 = CCMenu.menu(startGame);
-        menu2.alignItemsVertically(-10f);
-        menu2.setPosition(CGPoint.ccp(-20, 0));
-        menu2.setAnchorPoint(CGPoint.ccp(0, 0));
-        // addChild(menu2);
+        addChild(menuItem, 0);
+    }
 
-        ccStartNode.addChild(menu2);
+    public void startGame(Object id) {
+        Intent intent = new Intent(CCDirector.sharedDirector().getActivity(), GameActivity.class);
+        CCDirector.sharedDirector().getActivity().startActivity(intent);
     }
 }
