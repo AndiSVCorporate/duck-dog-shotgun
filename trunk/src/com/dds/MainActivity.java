37814dc2727dcd013361306c5c409867c94c8df2
t.c.nieuwenhuys@gmail.com <t.c.nieuwenhuys@gmail.com@bfb4acf6-1144-4334-fd50-431394f2b8de>
2012-10-08 16:51:36 +0000

    read/write files

--- trunk/src/com/dds/MainActivity.java
+++ trunk/src/com/dds/MainActivity.java
@@ -1,19 +1,24 @@
 package com.dds;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import org.cocos2d.layers.CCScene;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.opengl.CCGLSurfaceView;
 
 public class MainActivity extends Activity
 {
-
+	public static final String ROOT = Environment.getExternalStorageDirectory() + "/.BirdyBoom/";
     protected static CCGLSurfaceView mGLSurfaceView;
     protected int mode = 0;
     /** Called when the activity is first created. */
@@ -22,16 +27,23 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
 
+        if (!(new File(MainActivity.ROOT)).exists())
+        {
+        	File root = new File(MainActivity.ROOT);
+        	root.mkdirs();
+        }
+        
+        Log.e("Highscore", read("highscore.txt"));
+        
         // set the window status, no tile, full screen and don't sleep
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     }
 
-    public void onStart() {
+    public void onStart() 
+    {
         super.onStart();
 
         MainActivity.mGLSurfaceView = new CCGLSurfaceView(this);
@@ -41,7 +53,6 @@ public class MainActivity extends Activity
         // attach the OpenGL view to a window
         CCDirector.sharedDirector().attachInView(MainActivity.mGLSurfaceView);
 
-        // show FPS
         // set false to disable FPS display, but don't delete fps_images.png!!
         CCDirector.sharedDirector().setDisplayFPS(false);
 
@@ -52,7 +63,8 @@ public class MainActivity extends Activity
         CCDirector.sharedDirector().runWithScene(GameMenu.scene());
     }
 
-    public void onBackPressed() {
+    public void onBackPressed() 
+    {
     	if (mode == 0)
         {
     		onDestroy();
@@ -66,23 +78,59 @@ public class MainActivity extends Activity
     }
 
     @Override
-    public void onPause() {
+    public void onPause()
+    {
         super.onPause();
 
         CCDirector.sharedDirector().pause();
     }
 
     @Override
-    public void onResume() {
+    public void onResume() 
+    {
         super.onResume();
         
         CCDirector.sharedDirector().resume();
     }
 
     @Override
-    public void onDestroy() {
+    public void onDestroy() 
+    {
         super.onDestroy();
 
         CCDirector.sharedDirector().end();
+    }
+    
+    public String read(String file)
+    {
+    	String result = "";
+    	try
+    	{
+	    	FileInputStream fIn = openFileInput(file);
+	        InputStreamReader isr = new InputStreamReader(fIn);
+	        char[] inputBuffer = new char[1];
+	        
+	        while (isr.read(inputBuffer) != -1)
+	        {
+	        	result += new String(inputBuffer);
+	        }
+		} 
+	    catch (IOException e) {}
+    	
+    	return result;
+    }
+    
+    public void write(String file, String content)
+    {
+    	 try 
+    	 {
+             FileOutputStream fOut = openFileOutput(file, MODE_WORLD_READABLE);
+             OutputStreamWriter osw = new OutputStreamWriter(fOut); 
+
+             osw.write(content);
+             osw.flush();
+             osw.close();
+    	 }
+    	 catch (IOException e) {}
     }
 }
