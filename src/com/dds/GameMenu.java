3d5f5c7d83166f79d737567d4604abaf43e6abda
wildewouter <wildewouter@gmail.com>
2012-10-09 09:57:18 +0000

    FIXES

--- src/com/dds/GameMenu.java
+++ src/com/dds/GameMenu.java
@@ -12,7 +12,7 @@ import org.cocos2d.types.CGSize;
  * Time: 12:45
  */
 public class GameMenu extends CCMenu {
-	protected static CCLabel playLabel;
+	protected CCMenuItemImage menuItem;
 	
     public static CCScene scene() {
         CCScene returnScene = CCScene.node();
@@ -27,14 +27,27 @@ public class GameMenu extends CCMenu {
     public GameMenu() {
         super();
     	this.scheduleUpdate();
-        CGSize winSize = CCDirector.sharedDirector().displaySize();
 
-        CCMenuItemImage menuItem = CCMenuItemImage.item("button.png", "button_pressed.png", this, "startGame");
-        playLabel = CCLabel.makeLabel("Play Game", "Arial", 40f);
+        this.schedule("updateLabel", 0.1f);
+
+        menuItem = CCMenuItemImage.item("button.png", "button_pressed.png", this, "startGame");
+        CCLabel playLabel = CCLabel.makeLabel("Play Game", "Arial", 40f);
         playLabel.setPosition(menuItem.getContentSize().width/2, menuItem.getContentSize().height/2);
+        playLabel.setTag(1);
+
         menuItem.addChild(playLabel);
 
         addChild(menuItem, 0);
+    }
+
+    public void updateLabel(float dt) {
+        menuItem.removeChildByTag(1, true);
+
+        CCLabel playLabel = CCLabel.makeLabel("Play Game", "Arial", 40f);
+        playLabel.setPosition(menuItem.getContentSize().width/2, menuItem.getContentSize().height/2);
+        playLabel.setTag(1);
+
+        menuItem.addChild(playLabel);
     }
 
     public void startGame(Object id) {
@@ -44,6 +57,6 @@ public class GameMenu extends CCMenu {
     
     public void update(float time)
     {
-    	playLabel.setString("Play Game");
+//    	playLabel.setString("Play Game");
     }
 }
