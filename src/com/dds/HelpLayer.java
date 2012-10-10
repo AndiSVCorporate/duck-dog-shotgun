4d1fe3ae5e8426d3c05685eb8774ff79a94c85c4
wildewouter <wildewouter@gmail.com>
2012-10-10 16:38:01 +0000

    HelpLayer added

--- src/com/dds/HelpLayer.java
+++ src/com/dds/HelpLayer.java
@@ -1,11 +1,12 @@
 package com.dds;
 
+import android.view.MotionEvent;
+import org.cocos2d.events.CCTouchDispatcher;
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.layers.CCScene;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCLabel;
-import org.cocos2d.nodes.CCSprite;
-import org.cocos2d.types.CGPoint;
+import org.cocos2d.protocols.CCTouchDelegateProtocol;
 import org.cocos2d.types.CGSize;
 
 /**
@@ -13,7 +14,7 @@ import org.cocos2d.types.CGSize;
  * Date: 10-10-12
  * Time: 16:36
  */
-public class HelpLayer extends CCLayer {
+public class HelpLayer extends CCLayer implements CCTouchDelegateProtocol {
     public static CCScene scene() {
         CCScene returnScene = CCScene.node();
 
@@ -29,12 +30,25 @@ public class HelpLayer extends CCLayer {
     }
 
     public HelpLayer() {
-        CCLabel helpLabel = CCLabel.makeLabel("The purpose of Birdy Boom is to shoot " +
-                "and catch as much birds as you can. To shoot the birds, simply tap on the birds with your finger. " +
-                "To catch the falling birds tilt your device to move the catching animal. ","Arial", 18);
+        String texty = "The purpose of Birdy Boom is to shoot and catch as much birds as you can. " +
+                "To shoot the birds, simply tap on the birds with your finger. To catch the falling birds tilt your device to move " +
+                "the catching animal. Tap the screen to go to the menu.";
 
-        helpLabel.setPosition((int) GameLayer.dp2px(10), (int) GameLayer.dp2px(600));
+        CCTouchDispatcher.sharedDispatcher().addDelegate(this, 0);
+
+        CGSize dimensions = CGSize.make(600, 450);
+
+        CCLabel helpLabel = CCLabel.makeLabel(texty, dimensions, CCLabel.TextAlignment.LEFT,"Arial", 40);
+        helpLabel.setPosition(350, 700);
+
+        helpLabel.setScale(GameLayer.scale);
 
         addChild(helpLabel);
+    }
+
+    public synchronized boolean ccTouchesBegan(MotionEvent e) {
+        CCTouchDispatcher.sharedDispatcher().removeAllDelegates();
+        CCDirector.sharedDirector().popScene();
+        return false;
     }
 }
